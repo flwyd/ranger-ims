b823495effba2672e06ea614c88b30375b4d3d3a
Wilfredo Sánchez <wsanchez@wsanchez.net>
2013-04-07 16:53:00 -0700

    Ping server prior to other connections; load after a successful ping.

--- Incidents/Incidents/HTTPDataStore.m
+++ Incidents/Incidents/HTTPDataStore.m
@@ -28,6 +28,11 @@
 
 @property (strong) NSURL *url;
 
+@property (assign) BOOL serverAvailable;
+
+@property (strong) NSURLConnection *pingConnection;
+@property (strong) NSMutableData   *pingData;
+
 @property (strong) NSURLConnection *loadIncidentNumbersConnection;
 @property (strong) NSMutableData   *loadIncidentNumbersData;
 
@@ -63,6 +68,8 @@
         self.allIncidentsByNumber   = [NSMutableDictionary dictionary];
         self.incidentETagsByNumber  = [NSMutableDictionary dictionary];
         self.incidentsNumbersToLoad = [NSMutableSet set];
+
+        self.serverAvailable = NO;
     }
     return self;
 }
@@ -79,16 +86,6 @@
     else {
         return self.allIncidentsByNumber.allValues;
     }
-}
-
-
-- (BOOL) load
-{
-    [self loadIncidentTypes];
-    [self loadRangers];
-    [self loadIncidents];
-    
-    return NO;
 }
 
 
@@ -157,14 +154,67 @@
 }
 
 
+@synthesize serverAvailable;
+
+
+- (BOOL) serverAvailable {
+    return serverAvailable;
+}
+
+
+- (void) setServerAvailable:(BOOL)available
+{
+    if (available != serverAvailable) {
+        serverAvailable = available;
+
+        if (available) {
+            NSLog(@"Server connection is available.");
+            [self loadIncidentTypes];
+            [self loadRangers];
+            [self loadIncidents];
+        }
+        else {
+            NSLog(@"Server connection is no longer available.");
+        }
+    }
+}
+
+
+- (void) pingServer
+{
+    @synchronized(self) {
+        if (! self.pingConnection) {
+            NSURLConnection *connection = [self getJSONConnectionForPath:@"ping/"];
+
+            if (connection) {
+                self.pingConnection = connection;
+                self.pingData = [NSMutableData data];
+            }
+            else {
+                performAlert(@"Unable to connect to server.");
+                self.serverAvailable = NO;
+            }
+        }
+    }
+}
+
+
+- (BOOL) load
+{
+    [self pingServer];
+
+    return NO;
+}
+
+
 - (NSURLConnection *) getJSONConnectionForPath:(NSString *)path
 {
     NSURL *url = [self.url URLByAppendingPathComponent:path];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    
+
     [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
     [request setHTTPBody:[NSData data]];
-    
+
     return [NSURLConnection connectionWithRequest:request delegate:self];
 }
 
@@ -179,9 +229,9 @@
                 self.loadIncidentNumbersConnection = connection;
                 self.loadIncidentNumbersData = [NSMutableData data];
             }
-        }
-        else {
-            performAlert(@"Unable to connect to load incident numbers.");
+            else {
+                performAlert(@"Unable to connect to load incident numbers.");
+            }
         }
     }
 }
@@ -189,33 +239,35 @@
 
 - (void) loadQueuedIncidents {
     @synchronized(self) {
-        if (self.loadIncidentConnection) {
-            //
-            // This shouldn't happen given how this code is wired up.
-            // Logging here in case that cases accidentally, in case it's a performance oopsie.
-            //
-            NSLog(@"Already loading incidents... we shouldn't be here.");
-        }
-        else {
-            NSString *path = nil;
-            for (NSNumber *number in self.incidentsNumbersToLoad) {
-                //NSLog(@"Loading queued incident: %@", number);
-                path = [NSString stringWithFormat:@"incidents/%@", number];
-                NSURLConnection *connection = [self getJSONConnectionForPath:path];
-
-                if (connection) {
-                    self.loadIncidentConnection = connection;
-                    self.loadIncidentData = [NSMutableData data];
-                    self.loadIncidentNumber = number;
-                    self.loadIncidentETag = nil;
-                }
-                else {
-                    NSLog(@"Unable to connect to load queued incident: %@", number);
-                }
-                break;
+        if (self.serverAvailable) {
+            if (self.loadIncidentConnection) {
+                //
+                // This shouldn't happen given how this code is wired up.
+                // Logging here in case that cases accidentally, in case it's a performance oopsie.
+                //
+                NSLog(@"Already loading incidents... we shouldn't be here.");
             }
-            if (! path) {
-                NSLog(@"Done loading queued incidents.");
+            else {
+                NSString *path = nil;
+                for (NSNumber *number in self.incidentsNumbersToLoad) {
+                    //NSLog(@"Loading queued incident: %@", number);
+                    path = [NSString stringWithFormat:@"incidents/%@", number];
+                    NSURLConnection *connection = [self getJSONConnectionForPath:path];
+
+                    if (connection) {
+                        self.loadIncidentConnection = connection;
+                        self.loadIncidentData = [NSMutableData data];
+                        self.loadIncidentNumber = number;
+                        self.loadIncidentETag = nil;
+                    }
+                    else {
+                        NSLog(@"Unable to connect to load queued incident: %@", number);
+                    }
+                    break;
+                }
+                if (! path) {
+                    NSLog(@"Done loading queued incidents.");
+                }
             }
         }
     }
@@ -224,15 +276,17 @@
 
 - (void) loadRangers {
     @synchronized(self) {
-        if (! self.loadRangersConnection) {
-            NSURLConnection *connection = [self getJSONConnectionForPath:@"rangers/"];
-            
-            if (connection) {
-                self.loadRangersConnection = connection;
-                self.loadRangersData = [NSMutableData data];
-            }
-            else {
-                NSLog(@"Unable to connect to load Rangers.");
+        if (self.serverAvailable) {
+            if (! self.loadRangersConnection) {
+                NSURLConnection *connection = [self getJSONConnectionForPath:@"rangers/"];
+                
+                if (connection) {
+                    self.loadRangersConnection = connection;
+                    self.loadRangersData = [NSMutableData data];
+                }
+                else {
+                    NSLog(@"Unable to connect to load Rangers.");
+                }
             }
         }
     }
@@ -307,7 +361,18 @@
         return result;
     };
     
-    if (connection == self.loadIncidentNumbersConnection) {
+    if (connection == self.pingConnection) {
+        //NSLog(@"Ping request got response: %@", response);
+        if (happyResponse()) {
+            [self.pingData setLength:0];
+        }
+        else {
+            performAlert(@"Unable to ping.");
+            self.pingData = nil;
+            self.serverAvailable = NO;
+        }
+    }
+    else if (connection == self.loadIncidentNumbersConnection) {
         //NSLog(@"Load incident numbers request got response: %@", response);
         if (happyResponse()) {
             [self.loadIncidentNumbersData setLength:0];
@@ -358,7 +423,11 @@
 
 - (void) connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
 {
-    if (connection == self.loadIncidentNumbersConnection) {
+    if (connection == self.pingConnection) {
+        //NSLog(@"Ping request got data: %@", data);
+        [self.pingData appendData:data];
+    }
+    else if (connection == self.loadIncidentNumbersConnection) {
         //NSLog(@"Load incident numbers request got data: %@", data);
         [self.loadIncidentNumbersData appendData:data];
     }
@@ -382,28 +451,38 @@
 
 - (void) connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
 {
-    if (connection == self.loadIncidentNumbersConnection) {
-        performAlert(@"Load incident numbers request failed: %@", error);
+    if (connection == self.pingConnection) {
+        self.pingConnection = nil;
+        self.pingData = nil;
+        self.serverAvailable = NO;
+        performAlert(@"Ping failed: %@", error.localizedDescription);
+        NSLog(@"Unable to connect to server: %@", error);
+    }
+    else if (connection == self.loadIncidentNumbersConnection) {
         self.loadIncidentNumbersConnection = nil;
         self.loadIncidentNumbersData = nil;
+        performAlert(@"Load incident numbers request failed: %@", error);
     }
     if (connection == self.loadIncidentConnection) {
-        performAlert(@"Load incident request failed: %@", error);
         self.loadIncidentConnection = nil;
         self.loadIncidentData = nil;
+        performAlert(@"Load incident request failed: %@", error);
     }
     else if (connection == self.loadRangersConnection) {
-        performAlert(@"Load Rangers request failed: %@", error);
         self.loadRangersConnection = nil;
         self.loadRangersData = nil;
+        performAlert(@"Load Rangers request failed: %@", error);
     }
     else if (connection == self.loadIncidentTypesConnection) {
-        performAlert(@"Load incident types request failed: %@", error);
         self.loadIncidentTypesConnection = nil;
         self.loadIncidentTypesData = nil;
+        performAlert(@"Load incident types request failed: %@", error);
     }
     else {
-        performAlert(@"Unknown connection: %@\nGot error: %@", connection, error);
+        // FIXME: This actually happens; unsure why.
+        // Might be because the same connection yields two errors, and we stop
+        // tracking it after the first error…
+        NSLog(@"Unknown connection: %@\nGot error: %@", connection, error);
     }
     
     // FIXME: do something useful
@@ -412,12 +491,39 @@
 
 - (void) connectionDidFinishLoading:(NSURLConnection *)connection
 {
-    if (connection == self.loadIncidentConnection) {
+    if (connection == self.pingConnection) {
+        //NSLog(@"Ping request completed.");
+        self.pingConnection = nil;
+        if (self.pingData) {
+            NSError *error = nil;
+            NSString *jsonACK = [NSJSONSerialization JSONObjectWithData:self.pingData
+                                                                options:NSJSONReadingAllowFragments
+                                                                  error:&error];
+
+            if (jsonACK) {
+                if ([jsonACK isEqualToString:@"ack"]) {
+                    self.serverAvailable = YES;
+                }
+                else {
+                    performAlert(@"Unexpected response to ping: %@", jsonACK);
+                    self.serverAvailable = NO;
+                }
+            }
+            else {
+                performAlert(@"Unable to deserialize ping response: %@", error);
+            }
+        }
+
+        self.pingData = nil;
+    }
+    else if (connection == self.loadIncidentConnection) {
         //NSLog(@"Load incident request completed.");
         self.loadIncidentConnection = nil;
         if (self.loadIncidentData) {
             NSError *error = nil;
-            NSDictionary *jsonIncident = [NSJSONSerialization JSONObjectWithData:self.loadIncidentData options:0 error:&error];
+            NSDictionary *jsonIncident = [NSJSONSerialization JSONObjectWithData:self.loadIncidentData
+                                                                         options:0
+                                                                           error:&error];
             Incident *incident = [Incident incidentInDataStore:self fromJSON:jsonIncident error:&error];
 
             if (incident) {
@@ -433,7 +539,7 @@
                 }
             }
             else {
-                performAlert(@"Unable to load incident #%@: %@", self.loadIncidentNumber, error);
+                performAlert(@"Unable to deserialize incident #%@: %@", self.loadIncidentNumber, error);
             }
         }
 
