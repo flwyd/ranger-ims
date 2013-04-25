8cd854e67b5bce6fe687caa66b965ba76049de7e
Wilfredo Sánchez <wsanchez@wsanchez.net>
2013-04-24 18:35:32 -0700

    Add setup.py

---
+++ Server/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="Ranger IMS",
+    version="0.1",
+    packages=["ims",],
+    license="Apache License Version 2.0, January 2004",
+    long_description=open("../README").read(),
+)
