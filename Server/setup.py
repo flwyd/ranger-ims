a8073ab1331feaa488dcac8823ed992e9fb28e2e
Wilfredo Sánchez <wsanchez@wsanchez.net>
2013-06-24 18:09:29 -0700

    Add classifiers.

--- Server/setup.py
+++ Server/setup.py
@@ -3,18 +3,25 @@
 from distutils.core import setup
 
 setup(
-    name="ranger-ims",
-    version="0.1",
+    name = "ranger-ims",
+    version = "0.1",
 
-    description="Ranger Incident Management System",
-   #long_description=open("README").read(),
-    url="https://github.com/burningmantech/ranger-ims",
+    description = "Ranger Incident Management System",
+   #long_description = open("README").read(),
+    url = "https://github.com/burningmantech/ranger-ims",
 
-    author=u"Wilfredo S\xe1nchez",
-    author_email="tool@burningman.com",
+    author = u"Wilfredo S\xe1nchez",
+    author_email = "tool@burningman.com",
 
-    license="Apache License Version 2.0, January 2004",
+    license = "Apache License Version 2.0, January 2004",
 
-    packages=["ims"],
-    scripts=["bin/imsd"],
+    packages = ["ims"],
+    scripts = ["bin/imsd"],
+
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2",
+        "Topic :: Office/Business",
+    ]
 )
