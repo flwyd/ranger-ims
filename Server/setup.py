6994abf3565b1145c473b01b7ad8d4f8c411bcf5
Wilfredo Sánchez <wsanchez@wsanchez.net>
2013-06-24 17:54:44 -0700

    Update setup.py.

--- Server/setup.py
+++ Server/setup.py
@@ -3,7 +3,16 @@ from distutils.core import setup
 setup(
     name="Ranger IMS",
     version="0.1",
-    packages=["ims",],
+
+    description="Ranger Incident Management System",
+   #long_description=open("README").read(),
+    url="https://github.com/burningmantech/ranger-ims",
+
+    author=u"Wilfredo S\xe1nchez",
+    author_email="tool@burningman.com",
+
     license="Apache License Version 2.0, January 2004",
-    long_description=open("../README.md").read(),
+
+    packages=["ims"],
+    scripts=["bin/imsd"],
 )
